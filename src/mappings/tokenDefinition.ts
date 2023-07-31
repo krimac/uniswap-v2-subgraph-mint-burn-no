b69d0b1505e95778db49c3c314e68671c4d11c87
karamba <karambacoding@gmail.com>
2023-07-31 06:52:10 +0000

    array must be nullable

--- src/mappings/tokenDefinition.ts
+++ src/mappings/tokenDefinition.ts
@@ -21,7 +21,7 @@ export class TokenDefinition {
 
   // Get all tokens with a static defintion
   static getStaticDefinitions(): Array<TokenDefinition> {
-    let staticDefinitions = new Array<TokenDefinition>(6)
+    let staticDefinitions = new Array<TokenDefinition>(0)
 
     // Add DGD
     let tokenDGD = new TokenDefinition(
